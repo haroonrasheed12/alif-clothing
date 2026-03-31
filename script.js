fbc96adcfbe321f986930e671f1e8aa4e889e59a
haroonrasheed12 <haroonrash930@gmail.com>
2026-03-31 23:34:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Alif Cotton House Website Loaded 🚀");
+let cart = [];
+
+function addToCart(item) {
+  cart.push(item);
+  alert(item + " added to cart 🛒");
+}
+
+function filter(category) {
+  let items = document.querySelectorAll(".card");
+
+  items.forEach(card => {
+    if (category === "all") {
+      card.style.display = "inline-block";
+    } else {
+      card.style.display = card.classList.contains(category)
+        ? "inline-block"
+        : "none";
+    }
+  });
+}
+
+function orderNow() {
+  alert("Order placed! We will contact you 📞");
+}
